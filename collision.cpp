02d8dc1b74eebeb692344096c422920c41cd4d6f
BuhleN <sbongeka15@gmail.com>
2016-03-25 08:53:58 +0200

    Update collision.cpp

--- collision.cpp
+++ collision.cpp
@@ -1,20 +1,24 @@
 #include "collision.h"
 #include "math.h"
+#include <windows.h>  // for MS Windows
+#include <GL/glut.h>  // GLUT, include glu.h and gl.h
+#include <iostream>
+#include<math.h>
 collision::collision()
 {
     //ctor
 }
 
 
-double collision::calDistance(Obj o){
+/*double collision::calDistance(Obj o){
 double dist1= pow((o.x-b.x),2)+((o.y-b.y),2)+((o.z-b.z),2);
 distance=sqrt(dist1);
 return distance;
-}
+}*/
 void collision::detectCollision(Bee b){
-if(calDistance()+0.75==0){
+/*if(calDistance()+0.75==0){
     b.x++;
-}
+}*/
 if(b.x==by.x && b.y==by.y && b.z==by.z){
     b.x++;
     b.z--;
